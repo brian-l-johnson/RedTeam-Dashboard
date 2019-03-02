0c7bb1cd70a8095a94b55b9426fe7ebcab106411
brian-l-johnson <brian.l.johnson@gmail.com>
2019-03-01 18:51:48 -0600

    bug fixes

--- scanner/NmapScanWorker.js
+++ scanner/NmapScanWorker.js
@@ -6,6 +6,14 @@ const axios = require('axios').default;
 const axiosCookieJarSupport = require('axios-cookiejar-support').default;
 const tough = require('tough-cookie');
 
+var sync = require('child_process').spawnSync;
+
+const ipBlackList = ['10.0.0.0',
+                     '10.0.0.44',
+                    ];
+
+
+
 axiosCookieJarSupport(axios);
 
 const cookieJar = new tough.CookieJar();
@@ -14,11 +22,13 @@ axios.defaults.withCredentials = true;
 
 nmap.nmapLocation = "nmap"
 
+/*
 doWebAuth().then(function(data) {
     console.log("got "+data);
 });
+*/
 
-amqp.connect('amqp://localhost:32771', function(err, conn) {
+amqp.connect('amqp://scanner:Sc4nn3r-r4bb1t@dashboard.subversiveresearch.org:5672', function(err, conn) {
     conn.createChannel(function(err, ch) {
         var q = 'nmap-scan';
 
@@ -28,6 +38,8 @@ amqp.connect('amqp://localhost:32771', function(err, conn) {
         ch.consume(q, function(msg) {
             //var secs = msg.content.toString().split('.').length - 1;
             //console.log(msg);
+            ip = randomIP();
+            sync('ifconfig', ['eth0:0', ip]);
             console.log(" [x] Received %s", msg.content.toString());
 
             msgJson = JSON.parse(msg.content.toString());
@@ -35,16 +47,51 @@ amqp.connect('amqp://localhost:32771', function(err, conn) {
             console.log(msgJson.team);
             console.log(msgJson.range);
 
-            var nmapscan = new nmap.NmapScan(msgJson.range, '');
+            var nmapscan = new nmap.NmapScan(msgJson.range, '-e eth0 -S '+ip);
 
             nmapscan.on('complete', function(data) {
                 console.log(JSON.stringify(data));
-                axios.post('http://127.0.0.1:3001/nmap/'+msgJson.team, data)
+                axios.get('https://dashboard.subversiveresearch.org/auth/permissions')
                 .then(function(resp) {
-                    console.log(resp.data);
-                    ch.ack(msg);
+                    console.log(resp.status)
                 })
-                
+                .catch(function(error) {
+                    console.log("in catch, trying to login");
+                    const loginReq = axios.post('https://dashboard.subversiveresearch.org/auth/login', {
+                        "email": "brian.l.johnson@gmail.com",
+                        "password": "foobar"
+                    });
+                    loginReq
+                    .then(function(response) {
+                        console.log(response.status);
+                        console.log("login successful");
+                    })
+                    .catch(function(error) {
+                        console.log("Login failed");
+                        process.exit(1);
+                    });
+
+                    return loginReq;
+                })
+                .then(function() {
+                    console.log("about to post results");
+                    axios.post('https://dashboard.subversiveresearch.org/nmap/'+msgJson.team, data)
+                    .then(function(resp) {
+                        console.log(resp.data);
+                        if(resp.status === 200) {
+                            ch.ack(msg);
+                        }
+                        else {
+                            process.exit(1);
+                        }
+                        
+                    })
+                    .catch(function(error) {
+                        //console.log(error);
+                        console.log("failed tp upload scan results");
+                        process.exit(1);
+                    })
+                } )                
             });
             
             nmapscan.on('error', function(error) {
@@ -57,9 +104,17 @@ amqp.connect('amqp://localhost:32771', function(err, conn) {
     }, {noAck: false});
 });
 
+function randomIP() {
+    while(ip = "10."+Math.floor(Math.random() * 255)+"."+Math.floor(Math.random() * 255)+"."+Math.floor(Math.random() * 255)) {
+        if(!ipBlackList.includes(ip)) {
+            return ip;
+        }
+    }
+}
+
 
 function doWebAuth() {
-        const request = axios.post('http://127.0.0.1:3001/auth/login', {
+        const request = axios.post('https://dashboard.subversiveresearch.org/auth/login', {
             "email": "brian.l.johnson@gmail.com",
             "password": "foobar"
         });
@@ -68,7 +123,7 @@ function doWebAuth() {
         .then(function(response) {
             console.log(response.data);
             console.log(cookieJar);
-            axios.get('http://127.0.0.1:3001/auth/permissions')
+            axios.get('https://dashboard.subversiveresearch.org/auth/permissions')
             .then(function(resp) {
                 console.log(resp.data);
                 return resp.data;
