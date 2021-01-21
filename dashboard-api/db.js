28592a7e4b63138df2af22917532a5a3cff84a3d
Brian Johnson <brian.l.johnson@gmail.com>
2021-01-20 20:13:02 -0600

    update db connection to retry connections

--- dashboard-api/db.js
+++ dashboard-api/db.js
@@ -2,5 +2,10 @@ require('dotenv').config();
 
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGOOSE_STRING);
+mongoose.connect(process.env.MONGOOSE_STRING, {
+    useNewUrlParser: true,
+    reconnectTries : 100,
+    reconnectInterval: 1000,
+    autoReconnect : true
+  });
 //mongoose.connect('mongodb://localhost/ccdc');
